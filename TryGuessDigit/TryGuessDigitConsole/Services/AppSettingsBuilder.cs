38cdb00737b98374c0e27e7c6843f116755d86d4
bigrunner <sadknighter@gmail.com>
2023-08-21 11:14:14 +0300

    feat: Optimized DIP

--- TryGuessDigit/TryGuessDigitConsole/Services/AppSettingsBuilder.cs
+++ TryGuessDigit/TryGuessDigitConsole/Services/AppSettingsBuilder.cs
@@ -4,23 +4,11 @@ namespace TryGuessDigitConsole.Services
 {
     public static class AppSettingsBuilder
     {
-        private static AppSettings? _appSettings;
-
-        public static AppSettings GetAppSettings()
+        public static void BuildConfig(IConfigurationBuilder builder)
         {
-            IConfigurationBuilder builder = new ConfigurationBuilder()
-                   .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json", optional: false);
-
-            IConfiguration config = builder.Build();
-
-            var guessTimesCount = Convert.ToInt32(config["GuessTimesCount"]);
-            var rangeStartItem = Convert.ToInt32(config["RangeStartItem"]);
-            var rangeEndItem = Convert.ToInt32(config["RangeEndItem"]);
-
-            _appSettings ??= new AppSettings(guessTimesCount, rangeStartItem, rangeEndItem);
-
-            return _appSettings;
+            builder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
         }
     }
 }
